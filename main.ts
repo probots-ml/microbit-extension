ed1c4b175d3176a067ef8df82bf7c1aaf141d124
probots-ml <87763103+probots-ml@users.noreply.github.com>
2024-03-11 13:35:25 -0300

    NameSpace configuration

--- main.ts
+++ main.ts
@@ -1,4 +1,12 @@
+//% weight=5 color=#ff8000 icon="\uf2db"
+//% groups="['Miscellaneous','Leds', 'Motors','Buzzer','Sensors', 'Actuators']"
 namespace probots{
+
+    /**
+     * Get the actual version
+     */
+    //% blockId=getVersion
+    //% block="Version"
     export function version(): string{
         return "1.24.03";
     }
