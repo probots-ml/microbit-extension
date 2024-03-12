eb6a2a66e2c489fe805065360964b7b4cc51bbc3
probots-ml <87763103+probots-ml@users.noreply.github.com>
2024-03-12 12:31:47 -0300

    RGB Orange time

--- main.ts
+++ main.ts
@@ -1009,18 +1009,20 @@ namespace probots {
                 break;
             case Colors_rgb.Green:
                 pins.digitalWritePin(DigitalPin.P15, 0);
-                pins.digitalWritePin(DigitalPin.P14, 1);
-                pins.digitalWritePin(DigitalPin.P13, 0);
-                break;
-            case Colors_rgb.Blue:
-                pins.digitalWritePin(DigitalPin.P15, 0);
                 pins.digitalWritePin(DigitalPin.P14, 0);
                 pins.digitalWritePin(DigitalPin.P13, 1);
                 break;
-            case Colors_rgb.Orange:
-                pins.digitalWritePin(DigitalPin.P15, 1);
+            case Colors_rgb.Blue:
+                pins.digitalWritePin(DigitalPin.P15, 0);
                 pins.digitalWritePin(DigitalPin.P14, 1);
                 pins.digitalWritePin(DigitalPin.P13, 0);
+                break;
+            case Colors_rgb.Orange:
+                //100% red 64.7% green 0% blue
+                pins.digitalWritePin(DigitalPin.P15, 1); //red
+                pins.digitalWritePin(DigitalPin.P14, 0); //blue
+                pins.analogWritePin(getAnalogPin(DigitalPin.P13), 665);
+                //pins.digitalWritePin(DigitalPin.P13, 0);
                 break;
             case Colors_rgb.Violet:
                 pins.digitalWritePin(DigitalPin.P15, 1);
