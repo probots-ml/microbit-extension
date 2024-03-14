f6f9ee4c00fdd5a187b75b7c23a1426998214299
probots-ml <87763103+probots-ml@users.noreply.github.com>
2024-03-14 14:13:02 -0300

    Cleaning library

--- TCS34725-RGB-Sensor.ts
+++ TCS34725-RGB-Sensor.ts
@@ -1,51 +1,5 @@
-//% weight=100 color=#0fbc11 icon=""
-namespace ML_TCS34725{
 
-/*
-    const ADDR = 0x29;
-    const APDS9960_ENABLE = 0x80;
-    const APDS9960_ATIME = 0x81;
-    const APDS9960_CONTROL = 0x8F;
-    const APDS9960_STATUS = 0x93;
-    const APDS9960_CDATAL = 0x94;
-    const APDS9960_RDATAL = 0x96;
-    const APDS9960_GDATAL = 0x98;
-    const APDS9960_BDATAL = 0x9A;
-    const APDS9960_GCONF4 = 0xAB;
-    const APDS9960_AICLEAR = 0xE7;
-*/
-
-    function i2cwrite(addr: number, reg: number, value: number) {
-        let buf = pins.createBuffer(2)
-        buf[0] = reg
-        buf[1] = value
-        pins.i2cWriteBuffer(addr, buf)
-    }
-
-    function i2cread(addr: number, reg: number) {
-        pins.i2cWriteNumber(addr, reg, NumberFormat.UInt8BE);
-        let val = pins.i2cReadNumber(addr, NumberFormat.UInt8BE);
-        return val;
-    }
-/*
-    //% blockId=tcs34725_init block="Init Color Sensor I2C"
-    //% group="Sensors"
-    export function initColorSensor(): any {
-        
-        i2cwrite(ADDR, APDS9960_ATIME, 252) // default inte time 4x2.78ms
-        i2cwrite(ADDR, APDS9960_CONTROL, 0x03) // todo: make gain adjustable
-        i2cwrite(ADDR, APDS9960_ENABLE, 0x00) // put everything off
-        i2cwrite(ADDR, APDS9960_GCONF4, 0x00) // disable gesture mode
-        i2cwrite(ADDR, APDS9960_AICLEAR, 0x00) // clear all interrupt
-        // power on
-        i2cwrite(ADDR, APDS9960_ENABLE, 0x01) // clear all interrupt
-        let tmp = i2cread(ADDR, APDS9960_ENABLE) | 0x2;
-        i2cwrite(ADDR, APDS9960_ENABLE, tmp);
-
-        return tmp;
-    }
-*/
-
+namespace probots{
     // Samuel Niederer
     // TCS34725 extension
     const TCS34725_I2C_ADDRESS = 0x29        //I2C address of the TCS34725 (Page 34)
@@ -198,7 +152,7 @@ namespace ML_TCS34725{
 
         //% blockId=tcs34725_init block="Init Color Sensor I2C"
         //% group="Sensors"
-        export function initSensor(): boolean {
+        export function initSensor(): void {
             //REGISTER FORMAT:   CMD | TRANSACTION | ADDRESS
             //REGISTER READ:     TCS34725_REGISTER_COMMAND (0x80) | TCS34725_REGISTER_ID (0x12)
             let device_id = RegisterHelper.readRegister8(TCS34725_I2C_ADDRESS, TCS34725_REGISTER_COMMAND | TCS34725_REGISTER_ID)
@@ -209,8 +163,6 @@ namespace ML_TCS34725{
             }
             else
                 isConnected = true;
-
-            return isConnected;
         }
 
         export function turnSensorOn(atime: TCS34725_ATIME) {
@@ -380,6 +332,5 @@ namespace ML_TCS34725{
 
             return color;
         }
-
 
 }
